9fecf9561a64c1d1031aea112ba3684956c75116
Rakesh07778777 <bossrakesh43@gmail.com>
2025-10-29 09:58:53 +0530

    Remove duplicate SCSS folder

--- FoodSite 2 SCSS/dist/app.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelectorAll('.footer__toggle')
-
-button.forEach((toggle) => {
-    toggle.addEventListener('click', () => {
-        let parent = toggle.closest('.footer__container-items')
-        parent.classList.toggle('active')
-    })
-})
